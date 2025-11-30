41b9a492776d17d0ca7d92546e1fdd5197898d85
fraljer <samadeka1999@gmail.com>
2025-11-30 15:03:51 +0800

    To Color4

--- Fraljer.Anatta.Framework/Graphics/Colour.cs
+++ Fraljer.Anatta.Framework/Graphics/Colour.cs
@@ -61,7 +61,15 @@ public struct Colour
             A / 255f
         );
     }
-    
+   public Color4 ToColor4() {
+       return new Color4(
+                R / 255f,
+                G / 255f,
+                B / 255f,
+                A / 255f
+            );
+   }
+    public static implicit operator Color4(Colour c) => c.ToColor4();
     #region colours
         /// <summary>
         /// Gets the system color with (R, G, B, A) = (255, 255, 255, 0).
