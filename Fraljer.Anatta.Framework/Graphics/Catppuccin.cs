a50121238dc898e1c28d7b72608c520fae4092df
fraljer <samadeka1999@gmail.com>
2025-11-30 15:03:30 +0800

    Colours.

---
+++ Fraljer.Anatta.Framework/Graphics/Catppuccin.cs
@@ -0,0 +1,87 @@
+namespace Fraljer.Anatta.Framework.Graphics;
+
+public static class Catppuccin {
+
+    public class Latte
+    { 
+        public static Colour Rosewater = new Colour("#dc8a78");
+        public static Colour Flamingo = new Colour("#dd7878");
+        public static Colour Pink = new Colour("#ea76cb");
+        public static Colour Mauve = new Colour("#8839ef");
+        public static Colour Red = new Colour("#d20f39");
+        public static Colour Maroon = new Colour("#e64553");
+        public static Colour Peach = new Colour("#fe640b");
+        public static Colour Yellow = new Colour("#df8e1d");
+        public static Colour Green = new Colour("#40a02b");
+        public static Colour Teal = new Colour("#179299");
+        public static Colour Sky = new Colour("#04a5e5");
+        public static Colour Sapphire = new Colour("#209fb5");
+        public static Colour Blue = new Colour("#1e66f5");
+        public static Colour Lavender = new Colour("#7287fd");
+        public static Colour Text = new Colour("#4c4f69");
+        public static Colour Subtext1 = new Colour("#5c5f77");
+        public static Colour Subtext0 = new Colour("#6c6f85");
+        public static Colour Crust = new Colour("#dce0e8");
+    }
+    public class Frappe {
+        public static Colour Rosewater = new Colour("#f2d5cf");
+        public static Colour Flamingo = new Colour("#eebebe");
+        public static Colour Pink = new Colour("#f4b8e4");
+        public static Colour Mauve = new Colour("#ca9ee6");
+        public static Colour Red = new Colour("#e78284");
+        public static Colour Maroon = new Colour("#ea999c");
+        public static Colour Peach = new Colour("#ef9f76");
+        public static Colour Yellow = new Colour("#e5c890");
+        public static Colour Green = new Colour("#a6d189");
+        public static Colour Teal = new Colour("#81c8be");
+        public static Colour Sky = new Colour("#99d1db");
+        public static Colour Sapphire = new Colour("#85c1dc");
+        public static Colour Blue = new Colour("#8caaee");
+        public static Colour Lavender = new Colour("#babbf1");
+        public static Colour Text = new Colour("#c6d0f5");
+        public static Colour Subtext1 = new Colour("#b5bfe2");
+        public static Colour Subtext0 = new Colour("#a5adce");
+        public static Colour Crust = new Colour("#232634");
+    }
+    public class Macchiato {
+        public static Colour Rosewater = new Colour("#f4dbd6");
+        public static Colour Flamingo = new Colour("#f0c6c6");
+        public static Colour Pink = new Colour("#f5bde6");
+        public static Colour Mauve = new Colour("#c6a0f6");
+        public static Colour Red = new Colour("#ed8796");
+        public static Colour Maroon = new Colour("#ee99a0");
+        public static Colour Peach = new Colour("#f5a97f");
+        public static Colour Yellow = new Colour("#eed49f");
+        public static Colour Green = new Colour("#a6da95");
+        public static Colour Teal = new Colour("#8bd5ca");
+        public static Colour Sky = new Colour("#91d7e3");
+        public static Colour Sapphire = new Colour("#7dc4e4");
+        public static Colour Blue = new Colour("#8aadf4");
+        public static Colour Lavender = new Colour("#b7bdf8");
+        public static Colour Text = new Colour("#cad3f5");
+        public static Colour Subtext1 = new Colour("#b8c0e0");
+        public static Colour Subtext0 = new Colour("#a5adcb");
+        public static Colour Crust = new Colour("#181926");
+    }
+    public class Mocha {
+
+        public static Colour Rosewater = new Colour("#f5e0dc");
+        public static Colour Flamingo = new Colour("#f2cdcd");
+        public static Colour Pink = new Colour("#f5c2e7");
+        public static Colour Mauve = new Colour("#cba6f7");
+        public static Colour Red = new Colour("#f38ba8");
+        public static Colour Maroon = new Colour("#eba0ac");
+        public static Colour Peach = new Colour("#fab387");
+        public static Colour Yellow = new Colour("#f9e2af");
+        public static Colour Green = new Colour("#a6e3a1");
+        public static Colour Teal = new Colour("#94e2d5");
+        public static Colour Sky = new Colour("#89dceb");
+        public static Colour Sapphire = new Colour("#74c7ec");
+        public static Colour Blue = new Colour("#89b4fa");
+        public static Colour Lavender = new Colour("#b4befe");
+        public static Colour Text = new Colour("#cdd6f4");
+        public static Colour Subtext1 = new Colour("#bac2de");
+        public static Colour Subtext0 = new Colour("#a6adc8");
+        public static Colour Crust = new Colour("#11111b");
+    }
+}
