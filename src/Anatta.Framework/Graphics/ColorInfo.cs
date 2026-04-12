3ad5790e17bad949ed0ed20255b51a96f6c73df2
fråljer <clfraljer@gmail.com>
2026-04-12 14:31:36 +0800

    I don't even know bro

---
+++ src/Anatta.Framework/Graphics/ColorInfo.cs
@@ -0,0 +1,54 @@
+﻿using OpenTK.Mathematics;
+
+namespace Anatta.Framework.Graphics;
+
+public partial struct ColourInfo
+{
+    public Color Top;
+    public Color Bottom;
+    
+    // MAYBE I could replace colours with ColourInfo? MAYBE
+    public ColourInfo(Color single) {
+        Top = Bottom = single;
+    }
+
+    public ColourInfo(Color top, Color bottom) {
+        Top = top;
+        Bottom = bottom;
+    }
+
+    public static ColourInfo GradientVertical(Color top, Color bottom)
+        => new ColourInfo(top, bottom);
+
+    public static ColourInfo GradientHorizontal(Color left, Color right)
+        => new ColourInfo(left, right);
+    
+    public Vector4 ToVector4(float t) {
+        Color c = AnimationHelper.Lerp(Top, Bottom, t);
+        return new Vector4(c.R / 255f, c.G / 255f, c.B / 255f, c.A / 255f);
+    }
+    
+    public Color Darken(float factor = 0.1f)
+    {
+        factor = Math.Clamp(factor, 0f, 1f);
+        return new Color(
+            R * (1 - factor),
+            G * (1 - factor),
+            B * (1 - factor),
+            A
+        );
+    }
+
+    public ColorInfo Lighten(float factor = 0.1f) {
+        factor = Math.Clamp(factor, 0f, 1f);
+        return new Color(
+            R + (255 - R) * factor,
+            G + (255 - G) * factor,
+            B + (255 - B) * factor,
+            A
+        );
+    }
+    public Color Alpha(float alpha) {
+        return new Color(R, G, B, alpha);
+    }
+}
--- src/Anatta.Framework/Interfaces/Graphics/IHasGradients.cs
+++
@@ -1,9 +0,0 @@
-﻿using Anatta.Framework.Graphics;
-
-namespace Anatta.Framework.Graphics.Interfaces;
-
-public interface IHasGradients {
-    ColourInfo? Gradient { get; set; }
-
-    ColourInfo? BorderGradient { get; set; }
-}
